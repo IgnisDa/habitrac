113ded316e6b5620ffb237753faf7e8dd054f3c8
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-04 23:07:01 +0550

    feat(urls.py): add routing for graphql view

--- habitrac/habitrac/urls.py
+++ habitrac/habitrac/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from habitrac.api.graphql_config import HabitracGraphQLView, schema
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("graphql/", HabitracGraphQLView.as_view(schema=schema), name="graphql"),
 ]
