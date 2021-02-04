2d18a0153ef2019ec669daeef1ae8348635e8648
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-04 11:22:26 +0550

    feat(managers.py): remove email validation

--- habitrac/accounts/managers.py
+++ habitrac/accounts/managers.py
@@ -4,29 +4,20 @@ from validate_email import validate_email
 
 
 class CustomUserManager(BaseUserManager):
-    """
-    Custom user model manager where email is the unique identifiers
-    for authentication instead of usernames.
-    """
+    """Custom user model manager where username is the unique identifiers for
+    authentication ."""
 
-    def create_user(self, email, password, **extra_fields):
-        """
-        Create and save a User with the given email and password.
-        """
+    def create_user(self, username, password, **extra_fields):
+        """        Create and save a User with the given username and password.        """
 
-        if not email:
-            raise ValueError(_("The Email must be set"))
-        email = self.normalize_email(email)
-        if not validate_email(email):
-            raise ValueError(_("Invalid email set"))
-        user = self.model(email=email, **extra_fields)
+        user = self.model(username=username, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, username, password, **extra_fields):
         """
-        Create and save a SuperUser with the given email and password.
+        Create and save a SuperUser with the given username and password.
         """
 
         extra_fields.setdefault("is_staff", True)
@@ -37,4 +28,4 @@ class CustomUserManager(BaseUserManager):
             raise ValueError(_("Superuser must have is_staff=True."))
         if extra_fields.get("is_superuser") is not True:
             raise ValueError(_("Superuser must have is_superuser=True."))
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(username, password, **extra_fields)
