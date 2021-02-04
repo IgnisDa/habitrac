8250685090ae0739f740a4cfdb8ae545391ddb8e
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-04 23:07:56 +0550

    feat(habits): add habits app

---
+++ habitrac/habits/apps.py
@@ -0,0 +1,14 @@
+from django.apps import AppConfig
+from django.conf import settings
+from django.utils.autoreload import autoreload_started
+
+
+def schema_watchdog(sender, **kwargs):
+    sender.watch_dir(settings.BASE_DIR, "**/*.graphql")
+
+
+class HabitsConfig(AppConfig):
+    name = "habits"
+
+    def ready(self):
+        autoreload_started.connect(schema_watchdog)
