2d2e489246a0df4529f78e2ed02c469e35c00124
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-06 13:54:14 +0550

    feat(admin.py): register models to admin

--- habitrac/habits/admin.py
+++ habitrac/habits/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.HourlyHabit)
+class HourlyHabitAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.DailyHabit)
+class DailyHabitAdmin(admin.ModelAdmin):
+    pass
