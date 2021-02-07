c0410f0c2808cfb5f0853c79b711a1a01a2b6301
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-07 22:36:09 +0550

    feat(resolvers.py): add resolver for accounts app

---
+++ habitrac/accounts/api/resolvers.py
@@ -0,0 +1,19 @@
+import ariadne_jwt
+from ariadne import MutationType, QueryType
+
+auth_type_definitions = [
+    ariadne_jwt.jwt_schema,
+]
+
+jwt_mutation = MutationType()
+jwt_mutation.set_field("tokenAuth", ariadne_jwt.resolve_token_auth)
+jwt_mutation.set_field("refreshToken", ariadne_jwt.resolve_refresh)
+jwt_mutation.set_field("verifyToken", ariadne_jwt.resolve_verify)
+
+accounts_query = QueryType()
+
+
+@accounts_query.field("userDetail")
+@ariadne_jwt.decorators.login_required
+def user_detail(self, info, **kwargs):
+    return {"username": info.context.get("request").user.username}
