57e68e0edb5edd66cf40381c8c875a8702becf85
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-13 18:54:06 +0550

    feat(fontawesome.js): add fontawesome to project

---
+++ habitrac-frontend/plugins/fontawesome.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import { library, config } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  faUserGraduate,
+  faGuitar,
+  faUsers,
+  faClock,
+} from '@fortawesome/free-solid-svg-icons'
+
+// This is important, we are going to let Nuxt.js worry about the CSS
+config.autoAddCss = false
+
+// You can add your icons directly in this plugin. See other examples for how you
+// can add other styles or just individual icons.
+library.add(faUserGraduate, faClock, faGuitar, faUsers)
+
+// Register the component globally
+Vue.component('FontAwesomeIcon', FontAwesomeIcon)
