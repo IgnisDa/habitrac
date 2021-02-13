c019ce83aa639c0e92df015444601ed39c2575e3
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-13 11:56:23 +0550

    feat(tailwind.config.js): add @tailwind/forms

--- habitrac-frontend/tailwind.config.js
+++ habitrac-frontend/tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [require('tailwindcss-debug-screens')],
+  plugins: [
+    require('tailwindcss-debug-screens'),
+    require('@tailwindcss/forms'),
+  ],
 }
