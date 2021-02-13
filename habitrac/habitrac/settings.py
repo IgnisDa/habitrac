2e210120d9059c830c3c647e0e6642091784256d
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-13 17:38:23 +0550

    feat(settings.py): add middleware to check response time and authorization status

--- habitrac/habitrac/settings.py
+++ habitrac/habitrac/settings.py
@@ -137,6 +137,7 @@ if DEBUG:
     INSTALLED_APPS.append("django_extensions")
     SHELL_PLUS = "ipython"
     RUNSERVERPLUS_SERVER_ADDRESS_PORT = "0.0.0.0:8000"
+    MIDDLEWARE.append("accounts.middlewares.StatsMiddleware")
 if os.environ.get("DJANGO_TESTING", False) == "1":
     DATABASES = {
         "default": {
