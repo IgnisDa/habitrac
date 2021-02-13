d9de01d04ac8a08c542b2acbbe628486ddb99626
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-13 11:55:46 +0550

    feat(login.vue): page to handle login a user

---
+++ habitrac-frontend/pages/login.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container bg-red-100">
+    <form @submit.prevent="onSubmit()">
+      <input
+        v-model="credentials.username"
+        type="text"
+        placeholder="username"
+      />
+      <input
+        v-model="credentials.password"
+        type="password"
+        placeholder="password"
+      />
+      <input type="submit" value="Login" />
+    </form>
+  </div>
+</template>
+
+<script>
+import tokenAuthMutation from '~/apollo/mutations/tokenAuth.gql'
+
+export default {
+  data: () => ({
+    credentials: {
+      username: '',
+      password: '',
+    },
+  }),
+  methods: {
+    async onSubmit() {
+      const credentials = this.credentials
+      try {
+        await this.$apollo
+          .mutate({
+            mutation: tokenAuthMutation,
+            variables: {
+              username: credentials.username,
+              password: credentials.password,
+            },
+          })
+          .then(({ data }) => {
+            this.$apolloHelpers.onLogin(data.tokenAuth.token)
+          })
+      } catch (e) {
+        alert('Invalid credentials!')
+      }
+    },
+  },
+}
+</script>
