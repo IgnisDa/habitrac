bf7c57337a8e6aeeac227a9eb7267ffd1bd55ae1
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-14 22:01:15 +0550

    fix(nuxt.config.js): change meta tags to prevent problems on mobile screens

--- habitrac-frontend/nuxt.config.js
+++ habitrac-frontend/nuxt.config.js
@@ -4,7 +4,10 @@ export default {
     title: 'habitrac-frontend',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, minimum-scale=1',
+      },
       {
         hid: 'description',
         name: 'description',
