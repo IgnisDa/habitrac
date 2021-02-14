a59b308eabe5f551123727e4793fecdb1d9e7740
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-14 13:18:50 +0550

    test(middlewares.py): add a DEBUG only middleware which adds a 2 second response delay

--- habitrac/accounts/middlewares.py
+++ habitrac/accounts/middlewares.py
@@ -1,5 +1,7 @@
 import time
 
+from django.conf import settings
+
 
 class StatsMiddleware:
     """This middleware will add a header to the response to tell how much time
@@ -10,7 +12,6 @@ class StatsMiddleware:
 
     def __call__(self, request):
         start_time = time.time()
-        print(request.headers)
 
         response = self.get_response(request)
         response["Debug-Authorization"] = request.headers.get("Authorization", None)
@@ -19,4 +20,18 @@ class StatsMiddleware:
 
         # Add the header for monitoring metric of page generation times
         response["X-Page-Generation-Duration-ms"] = int(duration * 1000)
+        return response
+
+
+class DelayResponseMiddleware:
+    """This middleware will add a delay of `settings.DEBUG_RESPONSE_DELAY` before
+    sending back the response to each request. Mostly used to test loading animations
+    on the frontend client."""
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        time.sleep(int(settings.DEBUG_RESPONSE_DELAY))
+        response = self.get_response(request)
         return response
