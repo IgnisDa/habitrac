df8a452450c3af7a794924dde9139059788482e7
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-14 21:59:03 +0550

    feat(_id.vue): add file to display a particular user's info

---
+++ habitrac-frontend/pages/users/_id.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>{{ user }}</div>
+</template>
+
+<script>
+import userProfileDetailsQuery from '~/apollo/queries/userProfileDetails.gql'
+
+export default {
+  async asyncData({ app, params, error }) {
+    const apolloClient = app.apolloProvider.defaultClient
+    return await apolloClient
+      .query({
+        query: userProfileDetailsQuery,
+        variables: {
+          id: Number(params.id),
+        },
+      })
+      .then(({ data }) => {
+        console.log(data)
+        return { user: data.userProfileDetails.username }
+      })
+  },
+}
+</script>
