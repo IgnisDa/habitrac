d7be230a6d06cc9afcbb2f070aa1454d7010d6ab
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-14 17:19:53 +0550

    feat(NavBar.vue): add different link colors for inactive elements

--- habitrac-frontend/components/NavBar.vue
+++ habitrac-frontend/components/NavBar.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="fixed inset-x-0 bottom-0 w-full">
-    <div
-      class="flex justify-around w-full mx-auto font-mono text-sm font-light uppercase sm:w-9/12 md:w-7/12"
-    >
+    <div class="flex justify-around w-full mx-auto sm:w-9/12 md:w-7/12">
       <div v-for="(link, index) in links" :key="index">
         <NuxtLink
           :to="{ name: link.pathName }"
           :class="{ 'website-link': routeActive(link.pathName) }"
         >
-          <div class="px-1 pt-1">{{ link.label }}</div>
+          <div
+            class="px-3 pt-1 font-mono text-sm uppercase sm:text-lg sm:tracking-widest"
+          >
+            {{ link.label }}
+          </div>
         </NuxtLink>
       </div>
     </div>
@@ -34,25 +36,15 @@ export default {
 </script>
 
 <style scoped>
-.nuxt-link-exact-active::before {
-  content: '';
-  display: block;
-  margin: auto;
-  height: 3px;
-  width: 100%;
-  background: blue;
-  transition: width 0.3s ease, background-color 0.3s ease;
-}
+.nuxt-link-exact-active::before,
 .website-link::before {
   content: '';
-  width: 0;
-  display: block;
-  margin: auto;
-  height: 3px;
-  transition: width 0.3s ease, background-color 0.3s ease;
+  @apply block m-auto h-0.5 rounded-md w-full bg-purple-700 transition-width duration-200;
+}
+.website-link::before {
+  @apply w-0;
 }
 .website-link:hover::before {
-  width: 100%;
-  background: blue;
+  @apply w-full bg-pink-500;
 }
 </style>
