8a8db7794b96bd179ab244173ac35e1550d3400f
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-14 11:13:29 +0550

    feat(NavBar.vue): add component file to handle navbar

--- habitrac-frontend/components/NavBar.vue
+++ habitrac-frontend/components/NavBar.vue
@@ -1,3 +1,8 @@
 <template>
-  <div>Navbar</div>
+  <div class="fixed bottom-0 flex w-3/5 pb-4 bg-blue-500">
+    <div>Home</div>
+    <div>Register</div>
+    <div>Logout</div>
+    <div>Login</div>
+  </div>
 </template>
