26bc0fa9747182568c26c1aca269f36fc4fe02bb
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-15 23:35:46 +0550

    feat(fontawesome.js): import dark and light mode icons

--- habitrac-frontend/plugins/fontawesome.js
+++ habitrac-frontend/plugins/fontawesome.js
@@ -5,16 +5,28 @@ import {
   faUserGraduate,
   faSpinner,
   faGuitar,
+  faMoon,
+  faLightbulb,
   faUsers,
   faClock,
 } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 // This is important, we are going to let Nuxt.js worry about the CSS
 config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(faUserGraduate, faClock, faGuitar, faUsers, faSpinner)
+library.add(
+  faUserGraduate,
+  faClock,
+  faGuitar,
+  faUsers,
+  faSpinner,
+  faLightbulb,
+  faMoon,
+  faGithub
+)
 
 // Register the component globally
 Vue.component('FontAwesomeIcon', FontAwesomeIcon)
