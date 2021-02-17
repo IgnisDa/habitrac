818c3f5988fc342a6b6c3c01567ed24f6316f9a6
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-17 20:47:42 +0550

    feat(nuxt.config.js): remove ref to css file

--- habitrac-frontend/nuxt.config.js
+++ habitrac-frontend/nuxt.config.js
@@ -19,7 +19,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['~/assets/css/main.css', '~/assets/css/main.scss'],
+  css: ['~/assets/css/global.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: ['~/plugins/fontawesome.js'],
