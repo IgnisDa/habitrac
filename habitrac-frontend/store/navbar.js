600112ad9b73a0170da3d51b92c4666aca36c591
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-19 21:20:10 +0550

    feat(navbar.js): manage navbar elements using vuex

---
+++ habitrac-frontend/store/navbar.js
@@ -0,0 +1,41 @@
+export const state = () => ({
+  elements: [],
+  // the `authenticated` key decides whether the link will be displayed in the navbar
+  // according to authentication status
+  // --> `null`: displayed always
+  // --> `false`: displayed if the user is not logged in otherwise not
+  // --> `true`: displayed if the user is logged in otherwise not
+  allElements: [
+    { label: 'Home', pathName: 'index', authenticated: null },
+    { label: 'Register', pathName: 'register', authenticated: false },
+    { label: 'Login', pathName: 'login', authenticated: false },
+    { label: 'Logout', pathName: 'logout', authenticated: true },
+  ],
+})
+
+export const mutations = {
+  setNavbar(state, elements) {
+    state.elements = elements
+  },
+}
+
+export const actions = {
+  constructNavbarElements({ state, commit }) {
+    const links = []
+    const hasToken = this.app.$verifyAuthTokenExists()
+    for (const link of state.allElements) {
+      if (link.authenticated === null) {
+        links.push(link)
+      } else if (link.authenticated === false) {
+        if (!hasToken) {
+          links.push(link)
+        }
+      } else if (link.authenticated === true) {
+        if (hasToken) {
+          links.push(link)
+        }
+      }
+    }
+    commit('setNavbar', links)
+  },
+}
