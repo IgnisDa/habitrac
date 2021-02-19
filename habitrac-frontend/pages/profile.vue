ce26505527a910f2b5e21aec42f4c60326a1d63e
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-19 23:44:42 +0550

    feat(profile.vue): add restricted profile page

---
+++ habitrac-frontend/pages/profile.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <div>This is my profile page</div>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: ['isAuthenticated'],
+}
+</script>
