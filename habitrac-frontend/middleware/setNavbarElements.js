64bc6391db36e41ac54b42ddcc09cf66a8bf92f9
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-19 23:40:33 +0550

    docs(setNavbarElements.js): add comment to explain working

--- habitrac-frontend/middleware/setNavbarElements.js
+++ habitrac-frontend/middleware/setNavbarElements.js
@@ -1,3 +1,8 @@
 export default function ({ store }) {
+  // This middleware is run on every route to make sure that the navbar elements
+  // are always up-to-date. Though we could probably get away with running the
+  // `constructNavbarElements` action only on login and logout routes, it would
+  // introduce inconsistent behaviour if the user refreshes the website for any
+  // reason.
   store.dispatch('navbar/constructNavbarElements')
 }
