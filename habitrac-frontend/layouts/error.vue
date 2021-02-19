89e0d18d55b5daa9a5181f6d815ef3af1058dd26
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-19 23:43:19 +0550

    feat(error.vue): add custom page to display errors

---
+++ habitrac-frontend/layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>
+      {{ error.statusCode }}
+    </h1>
+    <h2>{{ error.message }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: () => ({
+        statusCode: 404,
+        message: 'Page not found',
+      }),
+    },
+  },
+}
+</script>
