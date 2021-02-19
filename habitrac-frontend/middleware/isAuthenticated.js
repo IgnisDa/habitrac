5629468a2be468ced091c17231f3a3629d8e16c7
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-02-20 00:02:00 +0550

    feat(isAuthenticated.js): only run middleware on client side

--- habitrac-frontend/middleware/isAuthenticated.js
+++ habitrac-frontend/middleware/isAuthenticated.js
@@ -1,10 +1,12 @@
 export default ({ store, error }) => {
-  store.dispatch('user/verifyLoggedIn').then((resp) => {
-    if (!resp) {
-      error({
-        statusCode: 503,
-        message: 'You are not allowed to see this',
-      })
-    }
-  })
+  if (!process.server) {
+    store.dispatch('user/verifyLoggedIn').then((resp) => {
+      if (!resp) {
+        error({
+          statusCode: 503,
+          message: 'You are not allowed to see this',
+        })
+      }
+    })
+  }
 }
