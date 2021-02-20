ae932c1b24e8dbd6205235b30eb22f8e1d1ee49e
IgnisDa <ignisda2001@gmail.com>
2021-02-20 12:55:48 +0550

    feat(Alert.vue): add component to handle alerts

--- habitrac-frontend/components/Alert.vue
+++ habitrac-frontend/components/Alert.vue
@@ -1,27 +1,38 @@
 <template>
-  <div id="alert--template">
-    <div v-for="alert in activeAlertsGetter" :key="alert.id">
+  <div id="alert--area" class="fixed z-10">
+    <transition-group name="alert" tag="div">
       <div
-        v-show="alert.active"
-        :class="{
-          'bg-green-500': alert.severity === 'success',
-          'bg-blue-400': alert.severity === 'info',
-          'bg-red-500': alert.severity === 'danger',
-          hidden: !alert.active,
-        }"
+        v-for="alert in activeAlertsGetter"
+        :id="`alert-${alert.id}`"
+        :key="alert.id"
+        class="my-3"
       >
-        <div class="alert__heading">{{ alert.messageHeading }}</div>
-        <div class="alert__body">
-          {{ alert.messageBody }}
-        </div>
-        <div class="cursor-pointer" @click="closeAlert(alert.id)">
+        <div
+          v-show="alert.active"
+          class="flex p-3 space-x-3 shadow-xl rounded-r-xl"
+          :class="{
+            'bg-green-400': alert.severity === 'success',
+            'bg-blue-400': alert.severity === 'info',
+            'bg-yellow-400': alert.severity === 'warning',
+            'bg-red-500': alert.severity === 'danger',
+          }"
+        >
+          <div class="block space-y">
+            <div class="block text-lg font-semibold underline">
+              {{ alert.messageHeading }}
+            </div>
+            <div class="block text-sm">
+              {{ alert.messageBody }}
+            </div>
+          </div>
           <FontAwesomeIcon
-            class="w-8 h-8"
-            :icon="['fas', 'times-circle']"
+            class="w-6 h-6 text-black cursor-pointer fill-current hover:text-gray-500"
+            :icon="['fas', 'times']"
+            @click="closeAlert(alert.id)"
           ></FontAwesomeIcon>
         </div>
       </div>
-    </div>
+    </transition-group>
   </div>
 </template>
 
@@ -50,3 +61,24 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.alert-leave-active,
+.alert-enter-active {
+  transition-duration: 1s;
+  transition-property: opacity, transform;
+  transform: translateX(-40px);
+}
+.alert-leave-active {
+  position: absolute;
+}
+.alert-leave-to {
+  opacity: 0;
+}
+.alert-enter-to {
+  transform: translateX(0);
+}
+.alert-move {
+  transition: transform 1s;
+}
+</style>
