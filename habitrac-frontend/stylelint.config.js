a22c75be223d91045404a526d72b9d0ffe3941cc
IgnisDa <ignisda2001@gmail.com>
2021-02-20 21:19:50 +0550

    build(stylelint.config.js): add scss related rules

--- habitrac-frontend/stylelint.config.js
+++ habitrac-frontend/stylelint.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier',
+    'stylelint-config-recommended-scss',
+    'stylelint-config-sass-guidelines',
+  ],
   rules: {
-    'at-rule-no-unknown': [
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': [
       true,
       {
         ignoreAtRules: [
@@ -17,5 +21,7 @@ module.exports = {
     ],
     'declaration-block-trailing-semicolon': null,
     'no-descending-specificity': null,
+    'max-nesting-depth': 3,
   },
+  plugins: ['stylelint-scss'],
 }
