2ca9eec9cbe94f8acd92409f31f375dc7eec570e
IgnisDa <ignisda2001@gmail.com>
2021-02-20 12:56:01 +0550

    fix(isAuthenticated.js): remove useless if statement

--- habitrac-frontend/middleware/isAuthenticated.js
+++ habitrac-frontend/middleware/isAuthenticated.js
@@ -1,12 +1,10 @@
 export default ({ store, error }) => {
-  if (!process.server) {
-    store.dispatch('user/verifyLoggedIn').then((resp) => {
-      if (!resp) {
-        error({
-          statusCode: 503,
-          message: 'You are not allowed to see this',
-        })
-      }
-    })
-  }
+  store.dispatch('user/verifyLoggedIn').then((resp) => {
+    if (!resp) {
+      error({
+        statusCode: 401,
+        message: 'You are not allowed to see this page',
+      })
+    }
+  })
 }
