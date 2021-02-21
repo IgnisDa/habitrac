b85099131225b878c800a1adb434e2e66a1a9f78
IgnisDa <ignisda2001@gmail.com>
2021-02-21 10:05:25 +0550

    feat(resolvers.py): add resolver to create a new habit

--- habitrac/habits/api/resolvers.py
+++ habitrac/habits/api/resolvers.py
@@ -1,5 +1,12 @@
-from ariadne import MutationType, QueryType, convert_kwargs_to_snake_case
+from ariadne import MutationType, QueryType
 from ariadne_jwt.decorators import login_required
 
 query = QueryType()
 mutation = MutationType()
+
+
+@mutation.field("createDailyHabit")
+# @login_required
+def create_daily_habit(*_, **data):
+    print(data)
+    return {"status": True}
