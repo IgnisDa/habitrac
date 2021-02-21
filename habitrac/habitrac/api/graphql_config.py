e010aacb1c9c3a54848020e9857ba7a856449278
IgnisDa <ignisda2001@gmail.com>
2021-02-21 10:03:39 +0550

    feat(graphql_config.py): add query and mutation resolvers for habit

--- habitrac/habitrac/api/graphql_config.py
+++ habitrac/habitrac/api/graphql_config.py
@@ -6,6 +6,8 @@ from ariadne.contrib.django.views import GraphQLView
 from django.conf import settings
 from habits.api import resolvers as habits_resolvers
 
+from . import resolvers as root_resolvers
+
 
 class HabitracGraphQLView(GraphQLView):
     pass
@@ -18,7 +20,9 @@ type_defs = [
 type_defs.extend(accounts_resolvers.auth_type_definitions)
 
 resolvers = [
+    root_resolvers.datetime_scalar,
     habits_resolvers.query,
+    habits_resolvers.mutation,
     accounts_resolvers.jwt_mutation,
     accounts_resolvers.accounts_query,
     accounts_resolvers.accounts_mutation,
