c1d0831486643751e5fa502639ae37d6b32e4069
IgnisDa <ignisda2001@gmail.com>
2021-02-24 10:57:48 +0550

    fix(resolvers.py): make error handling better, adapt to new schema

--- habitrac/habits/api/resolvers.py
+++ habitrac/habits/api/resolvers.py
@@ -9,24 +9,32 @@ mutation = MutationType()
 @mutation.field("createDailyHabit")
 @login_required
 def create_daily_habit(_, info, **data):
-    errors = []
+    errors = {}
     status = False
     user = info.context.get("request").user
     data = data["data"]
     name = data["name"]
     duration_from = data["duration"]["from"]
     duration_to = data["duration"]["to"]
-    duration = duration_to - duration_from
-    if duration_from > duration_to:
-        errors.append("The duration can not be negative")
+    duration_obj = duration_to - duration_from
+    duration = (duration_to - duration_from).total_seconds() // (3600 * 24)
+    if duration == 0:
+        errors.update(
+            {"duration": ["The starting date and the ending date can not be same"]}
+        )
         status = False
-    try:
-        if habit_models.DailyHabit.objects.filter(name__iexact=name, user=user).exists():
-            raise Exception("A habit with this name already exists")
-        habit_models.DailyHabit.objects.create(user=user, name=name, duration=duration)
-        status = True
-    except Exception as exc:
-        errors.append(str(exc))
+    elif duration < 0:
+        errors.update(
+            {"duration": ["The starting date can not be after the ending date"]}
+        )
+        status = False
+    if habit_models.DailyHabit.objects.filter(name__iexact=name, user=user).exists():
+        errors.update({"name": ["A habit with this name already exists"]})
+    else:
+        habit_models.DailyHabit.objects.create(
+            user=user, name=name, duration=duration_obj
+        )
     if not errors:
         errors = None
+        status = True
     return {"status": status, "errors": errors}
