a6cb15bd17bbdee6165fff2e9649e3c26b548579
IgnisDa <ignisda2001@gmail.com>
2021-02-24 13:02:50 +0550

    feat(index.vue): style page

--- habitrac-frontend/pages/users/index.vue
+++ habitrac-frontend/pages/users/index.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>{{ users }}</div>
+  <div class="flex items-center justify-center h-screen">
+    <ol
+      class="flex flex-col w-full p-3 mx-3 space-y-2 overflow-auto list-decimal list-inside bg-gray-200 divide-y divide-purple-200 rounded-lg shadow-lg sm:w-3/5 h-4/6 sm:h-5/6 sm:mx-0"
+    >
+      <li v-for="(user, index) in users" :key="index">
+        <NuxtLink
+          :to="{ name: 'users-slug', params: { slug: user.usernameSlug } }"
+        >
+          <span
+            class="text-blue-800 hover:text-blue-700 hover:underline sm:text-lg"
+            >{{ user.username }}</span
+          >
+        </NuxtLink>
+      </li>
+    </ol>
+  </div>
 </template>
 
 <script>
