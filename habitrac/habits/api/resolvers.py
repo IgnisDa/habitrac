d06d59554c6f8bb25a653d0ff5233b1be9521772
IgnisDa <ignisda2001@gmail.com>
2021-02-24 23:16:23 +0550

    feat(resolvers.py): use error handler in schema

--- habitrac/habits/api/resolvers.py
+++ habitrac/habits/api/resolvers.py
@@ -1,9 +1,11 @@
+import datetime
 import json
 
 from ariadne import MutationType, QueryType, convert_kwargs_to_snake_case
 from ariadne_token_auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from habits import models as habit_models
+from utils.handlers.errors import ErrorContainer
 
 CUSTOM_USER_MODEL = get_user_model()
 query = QueryType()
@@ -13,35 +15,39 @@ mutation = MutationType()
 @mutation.field("createDailyHabit")
 @login_required
 def create_daily_habit(_, info, **data):
-    errors = {}
+    error_container = ErrorContainer("duration", "name")
     status = False
     user = info.context.get("request").user
     data = data["data"]
     name = data["name"]
     duration_from = data["duration"]["from"]
     duration_to = data["duration"]["to"]
+    now = datetime.datetime.now()
+    if duration_from < now:
+        error_container.update_with_error(
+            "duration",
+            f"The starting date must be greater than {now.strftime('%m/%d/%Y')}",
+        )
     duration_obj = duration_to - duration_from
     duration = (duration_to - duration_from).total_seconds() // (3600 * 24)
     if duration == 0:
-        errors.update(
-            {"duration": ["The starting date and the ending date can not be same"]}
+        error_container.update_with_error(
+            "duration", "The starting date and the ending date can not be same"
         )
-        status = False
     elif duration < 0:
-        errors.update(
-            {"duration": ["The starting date can not be after the ending date"]}
+        error_container.update_with_error(
+            "duration", "The starting date can not be after the ending date"
         )
-        status = False
     if habit_models.DailyHabit.objects.filter(name__iexact=name, user=user).exists():
-        errors.update({"name": ["A habit with this name already exists"]})
-    else:
+        error_container.update_with_error(
+            "name", "A habit with this name already exists"
+        )
+    if not error_container:
         habit_models.DailyHabit.objects.create(
             user=user, name=name, duration=duration_obj
         )
-    if not errors:
-        errors = None
         status = True
-    return {"status": status, "errors": errors}
+    return {"status": status, "errors": error_container.get_all_errors()}
 
 
 @query.field("getAllHabits")
