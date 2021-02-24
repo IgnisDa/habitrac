1963a11ac5f65ac8ef12f0fd28fc2ef53386ceae
IgnisDa <ignisda2001@gmail.com>
2021-02-24 22:37:23 +0550

    feat(admin.py): add name_slug readonly field

--- habitrac/habits/admin.py
+++ habitrac/habits/admin.py
@@ -5,9 +5,9 @@ from . import models
 
 @admin.register(models.HourlyHabit)
 class HourlyHabitAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ("name_slug",)
 
 
 @admin.register(models.DailyHabit)
 class DailyHabitAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ("name_slug",)
