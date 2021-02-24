71e692073e4ab91a9fc4c76a7fd20f0c682830cc
IgnisDa <ignisda2001@gmail.com>
2021-02-24 10:47:09 +0550

    feat(0002_customuser_username_slug.py, models.py): add field for user slug

--- habitrac/accounts/models.py
+++ habitrac/accounts/models.py
@@ -1,8 +1,25 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
 
 from . import managers
+
+
+def create_username_slug(username):
+    """This is a function that returns a unique random slug taking into
+    account whether a user with the generated slug already exists in the db."""
+    return_val = None
+    slug = slugify(username)
+    while True:
+        if not CustomUser.objects.filter(username_slug=slug).exists():
+            return_val = slug
+            break
+        else:
+            slug = slugify(username) + str(uuid.uuid4().hex.upper()[:6])
+    return return_val
 
 
 class CustomUser(AbstractUser):
@@ -10,6 +27,11 @@ class CustomUser(AbstractUser):
         max_length=150, help_text=_("The username of the user."), unique=True
     )
     email = None
+    username_slug = models.SlugField(
+        max_length=100,
+        help_text=_("The unique slug that will be used to identify the user"),
+        editable=False,
+    )
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = []
@@ -18,6 +40,11 @@ class CustomUser(AbstractUser):
 
     class Meta:
         ordering = ("id",)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.username_slug = create_username_slug(self.username)
+        super(CustomUser, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.username}'s account"
