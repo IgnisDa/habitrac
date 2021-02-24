b245a9eff1a4d6cadebfa4e8d9e87830a4cdbe2e
IgnisDa <ignisda2001@gmail.com>
2021-02-24 10:48:25 +0550

    feat(index.vue): page to display all users

---
+++ habitrac-frontend/pages/users/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ users }}</div>
+</template>
+
+<script>
+import getUsersListQuery from '~/apollo/queries/getUsersList.gql'
+
+export default {
+  apollo: {
+    users: {
+      query: getUsersListQuery,
+      update: (value) => {
+        return value.getUsersList
+      },
+    },
+  },
+}
+</script>
