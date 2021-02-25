55f29e0b868ac679a2992ac734873bd1a41d7605
IgnisDa <ignisda2001@gmail.com>
2021-02-25 21:11:56 +0550

    feat(HabitCycleBox.vue): add component to handle showing a habit

---
+++ habitrac-frontend/components/HabitCycleBox.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="p-0.5 cursor-pointer">
+    <div
+      :class="
+        cycle
+          ? 'bg-green-500 hover:bg-green-400'
+          : 'bg-red-500 hover:bg-red-400'
+      "
+      class="flex items-center justify-center w-8 h-8 transition-colors duration-300 rounded-sm"
+      :title="tooltipText"
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    cycle: {
+      type: Boolean,
+      default: false,
+    },
+    startedOn: {
+      type: String,
+      default: '',
+    },
+    tooltipText: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {},
+}
+</script>
