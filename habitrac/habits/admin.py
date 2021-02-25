7d775d77e31378494312f1ea7c32b4b93dd5dc21
IgnisDa <ignisda2001@gmail.com>
2021-02-25 21:08:12 +0550

    feat(admin.py): add progress to read only fields

--- habitrac/habits/admin.py
+++ habitrac/habits/admin.py
@@ -5,9 +5,9 @@ from . import models
 
 @admin.register(models.HourlyHabit)
 class HourlyHabitAdmin(admin.ModelAdmin):
-    readonly_fields = ("name_slug",)
+    readonly_fields = ("name_slug", "progress")
 
 
 @admin.register(models.DailyHabit)
 class DailyHabitAdmin(admin.ModelAdmin):
-    readonly_fields = ("name_slug",)
+    readonly_fields = ("name_slug", "progress")
