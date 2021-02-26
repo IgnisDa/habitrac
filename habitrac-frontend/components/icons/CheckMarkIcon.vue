3b8d15cd4ef49ebc0827bf2e9d5fdccad986f0e4
IgnisDa <ignisda2001@gmail.com>
2021-02-26 22:22:23 +0550

    feat(CheckMarkIcon.vue): add check mark icon

---
+++ habitrac-frontend/components/icons/CheckMarkIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <FontAwesomeIcon
+      :icon="['fas', 'check']"
+      :class="classes"
+    ></FontAwesomeIcon>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    classes: {
+      type: String,
+      default: 'h-8 w-8',
+    },
+  },
+}
+</script>
