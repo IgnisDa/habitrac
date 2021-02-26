2b2603bc7a9c57f74ee8c22a88f498151c381f5e
IgnisDa <ignisda2001@gmail.com>
2021-02-26 11:42:44 +0550

    feat(index.vue): use new component to display habit

--- habitrac-frontend/pages/habits/_slug/index.vue
+++ habitrac-frontend/pages/habits/_slug/index.vue
@@ -2,10 +2,15 @@
   <div class="flex items-center justify-center h-screen">
     <div
       v-if="habit"
-      class="flex flex-col w-full p-3 mx-3 space-y-2 overflow-auto bg-gray-200 rounded-lg shadow-lg sm:w-3/5 h-4/6 sm:h-5/6 sm:mx-0"
+      class="flex flex-col w-full p-3 mx-3 space-y-2 overflow-auto bg-gray-200 rounded-lg shadow-lg sm:w-4/5 h-4/6 sm:h-5/6 sm:mx-0"
     >
       <div class="flex flex-wrap justify-around my-auto">
-        <div v-for="(value, _, index) in habit.progress" :key="index">
+        <HabitCycle
+          :cycle-index="cycleIndex"
+          :tagged="tagged"
+          @clicked="toggleTagCycle()"
+        ></HabitCycle>
+        <!-- <div v-for="(value, _, index) in habit.progress" :key="index">
           <HabitCycleBox
             :cycle="value"
             :started-on="habit.startedOn"
@@ -15,7 +20,7 @@
           >
             {{ parseDate(index).format('DD') }}
           </HabitCycleBox>
-        </div>
+        </div> -->
       </div>
     </div>
   </div>
@@ -26,7 +31,7 @@ import getHabitDetailsQuery from '~/apollo/queries/getHabitDetails.gql'
 import toggleTagCycleMutation from '~/apollo/mutations/toggleTagCycle.gql'
 
 export default {
-  async asyncData({ app, params }) {
+  async asyncData({ app, params, $dayjs }) {
     const apolloClient = app.apolloProvider.defaultClient
     const slug = params.slug
     const { data } = await apolloClient.query({
@@ -36,8 +41,15 @@ export default {
       },
     })
     if (data) {
+      const habit = data.getHabitDetails.habit
+      const date = $dayjs()
+      const startedOn = $dayjs(habit.startedOn)
+      const cycleIndex = date.diff(startedOn, 'd')
+      const key = `cycle-${cycleIndex}`
       const obj = {
-        habit: data.getHabitDetails.habit,
+        habit,
+        cycleIndex,
+        tagged: habit.progress[key],
       }
       if (typeof obj.habit.progress === 'string') {
         obj.habit.progress = JSON.parse(obj.habit.progress)
@@ -45,25 +57,21 @@ export default {
       return obj
     }
   },
+  data: () => ({ cycleIndex: null }),
   methods: {
-    parseDate(index) {
-      return this.$dayjs(this.habit.startedOn).add(index, 'day')
-    },
-    toggleTagCycle(index) {
-      // const today = new Date().getDate()
+    toggleTagCycle() {
       this.$apollo
         .mutate({
           mutation: toggleTagCycleMutation,
           variables: {
             data: {
               nameSlug: this.$route.params.slug,
-              cycleIndex: index,
+              cycleIndex: this.cycleIndex,
             },
           },
         })
         .then(() => {
-          const key = `cycle-${index + 1}`
-          this.habit.progress[key] = !this.habit.progress[key]
+          this.tagged = !this.tagged
         })
     },
   },
