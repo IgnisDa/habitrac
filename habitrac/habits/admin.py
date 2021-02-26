e08d6f3f4ecbf1f7323da4232520b60919a7ecd9
IgnisDa <ignisda2001@gmail.com>
2021-02-26 11:42:57 +0550

    feat(admin.py): add started_on to readonly fields

--- habitrac/habits/admin.py
+++ habitrac/habits/admin.py
@@ -5,9 +5,9 @@ from . import models
 
 @admin.register(models.HourlyHabit)
 class HourlyHabitAdmin(admin.ModelAdmin):
-    readonly_fields = ("name_slug", "progress")
+    readonly_fields = ("name_slug", "progress", "started_on")
 
 
 @admin.register(models.DailyHabit)
 class DailyHabitAdmin(admin.ModelAdmin):
-    readonly_fields = ("name_slug", "progress")
+    readonly_fields = ("name_slug", "progress", "started_on")
