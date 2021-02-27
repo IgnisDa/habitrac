df5ff2a08a2affbbb166ce562f38126ad3e4a998
IgnisDa <ignisda2001@gmail.com>
2021-02-27 19:17:29 +0550

    feat(admin.py): remove depreceated models

--- habitrac/habits/admin.py
+++ habitrac/habits/admin.py
@@ -3,11 +3,9 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.HourlyHabit)
-class HourlyHabitAdmin(admin.ModelAdmin):
-    readonly_fields = ("name_slug", "progress", "started_on")
-
-
 @admin.register(models.DailyHabit)
 class DailyHabitAdmin(admin.ModelAdmin):
-    readonly_fields = ("name_slug", "progress", "started_on")
+    readonly_fields = (
+        "name_slug",
+        # "progress",
+    )
