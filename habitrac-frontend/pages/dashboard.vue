d14cc7070746d8f49acec7a0b4d6001806173780
IgnisDa <ignisda2001@gmail.com>
2021-03-01 22:25:49 +0550

    feat(dashboard.vue): page to show profile options

---
+++ habitrac-frontend/pages/dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <div class="w-full mx-3 sm:w-4/5 md:w-1/3">
+      <CodeWindow>
+        <template #body>
+          <div class="hover:underline">
+            <NuxtLink :to="{ name: 'habits' }">My Habits</NuxtLink>
+          </div>
+          <div class="hover:underline">
+            <NuxtLink :to="{ name: 'profile' }">My Profile</NuxtLink>
+          </div>
+        </template>
+      </CodeWindow>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head: () => ({
+    title: 'Dashboard',
+  }),
+}
+</script>
