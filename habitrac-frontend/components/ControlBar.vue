986611ac6c3dedfbd06e6d0b01f8bc73b7d8d122
IgnisDa <ignisda2001@gmail.com>
2021-03-01 22:20:21 +0550

    feat(ControlBar.vue): remove dark and light icon

--- habitrac-frontend/components/ControlBar.vue
+++ habitrac-frontend/components/ControlBar.vue
@@ -1,51 +1,18 @@
 <template>
-  <div class="fixed top-0 left-0 right-0 flex mt-2 ml-1">
+  <div class="fixed top-0 right-0 m-3">
     <div class="flex justify-around w-full mx-auto lg:w-10/12 xl:w-8/12">
       <a href="//github.com/IgnisDa/habitrac" target="blank">
         <FontAwesomeIcon
-          class="w-8 h-8 sm:h-10 sm:w-10"
+          class="w-10 h-10 transition-colors duration-1000 fill-current sm:h-10 sm:w-10 text-true-gray-200 hover:text-true-gray-100"
           :icon="['fab', 'github']"
         ></FontAwesomeIcon>
       </a>
-      <transition name="color-mode" mode="out-in">
-        <component
-          :is="getColorModeIcon"
-          classes="h-8 w-8 sm:h-10 sm:w-10"
-          @click.native="toggleColorMode"
-        ></component>
-      </transition>
     </div>
   </div>
 </template>
 
 <script>
-import DarkModeIcon from '~/components/icons/DarkModeIcon.vue'
-import LightModeIcon from '~/components/icons/LightModeIcon.vue'
-
-export default {
-  components: {
-    DarkModeIcon,
-    LightModeIcon,
-  },
-  computed: {
-    getColorModeIcon() {
-      if (this.$colorMode.value === 'light') {
-        return 'LightModeIcon'
-      } else {
-        return 'DarkModeIcon'
-      }
-    },
-  },
-  methods: {
-    toggleColorMode() {
-      if (this.$colorMode.value === 'light') {
-        this.$colorMode.preference = 'dark'
-      } else {
-        this.$colorMode.preference = 'light'
-      }
-    },
-  },
-}
+export default {}
 </script>
 
 <style scoped>
