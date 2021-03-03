94b6f045aa84609dd059251b9375d8570be04a30
IgnisDa <ignisda2001@gmail.com>
2021-03-03 15:29:17 +0550

    feat(index.vue): finish touch ups

--- habitrac-frontend/pages/habits/_slug/index.vue
+++ habitrac-frontend/pages/habits/_slug/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center justify-center h-screen space-y-8">
     <div
-      class="px-5 py-2 font-serif text-3xl font-light border-2 border-red-500 rounded-md sm:text-5xl md:text-7xl"
+      class="px-5 py-2 font-serif text-3xl font-light border-2 border-yellow-300 rounded-md text-true-gray-200 sm:text-5xl md:text-7xl"
     >
       {{ habit.name }}
     </div>
@@ -10,13 +10,15 @@
         name: 'habits-slug-details',
         params: { slug: $route.params.slug },
       }"
-      class="relative flex items-center justify-center px-3 py-2 space-x-2 bg-blue-400 border border-red-500 rounded-md hover:bg-blue-300"
+      class="relative flex items-center justify-center px-3 py-2 space-x-2 transition-colors duration-300 bg-blue-400 border-yellow-300 rounded-md hover:bg-blue-300"
     >
       <FontAwesomeIcon
         class="w-6 h-6 sm:w-8 sm:h-8"
         :icon="['fas', 'info-circle']"
       ></FontAwesomeIcon>
-      <div class="font-mono text-lg italic sm:text-xl md:text-3xl">Details</div>
+      <div class="pt-1 text-lg font-display sm:text-xl md:text-3xl">
+        Details
+      </div>
     </NuxtLink>
     <div
       v-if="habit"
@@ -77,6 +79,9 @@ export default {
       } else return obj
     }
   },
+  head() {
+    return { title: `${this.habit.name} | ${this.cycle}` }
+  },
   methods: {
     toggleTagCycle() {
       this.$apollo
@@ -109,6 +114,6 @@ export default {
 <style scoped>
 a::after {
   content: '';
-  @apply w-0.5 bg-red-500 h-8 absolute -top-8;
+  @apply w-0.5 bg-yellow-300 h-8 absolute -top-8;
 }
 </style>
