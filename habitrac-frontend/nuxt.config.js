4f82f09710add8599923b77cee70c5c07f0f54d7
IgnisDa <ignisda2001@gmail.com>
2021-03-03 19:18:12 +0530

    add backend url to config

--- habitrac-frontend/nuxt.config.js
+++ habitrac-frontend/nuxt.config.js
@@ -72,7 +72,7 @@ export default {
   },
   publicRuntimeConfig: {
     backendUrl:
-      process.env.VUE_APP_BACKEND_URL || 'http://0.0.0.0:8000/graphql/',
+      process.env.VUE_APP_BACKEND_URL || 'https://habitrac.herokuapp.com/',
   },
   apollo: {
     clientConfigs: {
