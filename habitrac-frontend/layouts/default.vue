818c4b2ace59446c6444ff4efdfd6ece2be0fbcb
IgnisDa <ignisda2001@gmail.com>
2021-03-04 08:03:05 +0550

    feat(default.vue): change bg position on  small screens

--- habitrac-frontend/layouts/default.vue
+++ habitrac-frontend/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     :class="mode !== 'production' ? 'debug-screens' : ''"
-    class="min-h-screen antialiased bg-center bg-no-repeat bg-cover"
+    class="min-h-screen antialiased bg-left bg-no-repeat bg-cover sm:bg-center"
     :style="{
       backgroundImage: `linear-gradient(90deg, rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.15)), url('https://images.pexels.com/photos/1111318/pexels-photo-1111318.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')`,
     }"
