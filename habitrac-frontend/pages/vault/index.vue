b430768259c22c9030aea29b670bba458a23ee60
IgnisDa <ignisda2001@gmail.com>
2021-03-05 23:35:26 +0550

    feat(index.vue): page to display vault

---
+++ habitrac-frontend/pages/vault/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>Index</div>
+</template>
+
+<script>
+import { getCookie } from '~/utils.js'
+
+export default {
+  mounted() {
+    if (!getCookie('vault-token')) {
+      this.$router.push({ name: 'vault-prompt' })
+      this.$addAlert({
+        severity: 'danger',
+        messageBody:
+          'The vault token has expired, please login into the vault again.',
+        messageHeading: 'Expired',
+        active: true,
+      })
+    }
+  },
+}
+</script>
