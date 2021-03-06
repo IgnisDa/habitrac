08ebe75cd3de58067a5bdb494f87ca0d51dadeb5
IgnisDa <ignisda2001@gmail.com>
2021-03-06 09:10:53 +0550

    feat(index.vue): complete page

--- habitrac-frontend/pages/vault/index.vue
+++ habitrac-frontend/pages/vault/index.vue
@@ -1,12 +1,53 @@
 <template>
-  <div>Index</div>
+  <div class="flex items-center justify-center h-screen">
+    <div class="w-full mx-3 sm:w-4/5 md:w-1/3">
+      <CodeWindow>
+        <template #body>
+          <div v-if="loading">Loading...</div>
+          <div v-else>
+            <div v-if="habits.length > 0">
+              <div v-for="(habit, index) in habits" :key="index">
+                <NuxtLink
+                  :to="{
+                    name: 'habits-slug',
+                    params: { slug: habit.nameSlug },
+                  }"
+                >
+                  <span class="text-true-gray-200 hover:underline sm:text-lg">
+                    {{ habit.name }}
+                  </span>
+                </NuxtLink>
+              </div>
+            </div>
+            <div v-else class="text-center">
+              You have not created any habits to track. Create one
+              <NuxtLink
+                :to="{ name: 'habits-create' }"
+                class="text-blue-600 hover:underline"
+              >
+                here</NuxtLink
+              >.
+            </div>
+          </div>
+        </template>
+      </CodeWindow>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex'
+import getVaultHabitsQuery from '~/apollo/queries/getVaultHabits.gql'
 import { getCookie } from '~/utils.js'
 
 export default {
-  mounted() {
+  data: () => ({ habits: [], loading: false }),
+  computed: {
+    ...mapState({
+      usernameSlug: (state) => state.user.user.usernameSlug,
+    }),
+  },
+  async mounted() {
     if (!getCookie('vault-token')) {
       this.$router.push({ name: 'vault-prompt' })
       this.$addAlert({
@@ -16,7 +57,26 @@ export default {
         messageHeading: 'Expired',
         active: true,
       })
+    } else {
+      this.loading = true
+      await this.fetchUserDetailsAction()
+      await this.$apollo
+        .query({
+          query: getVaultHabitsQuery,
+          variables: {
+            usernameSlug: this.usernameSlug,
+          },
+        })
+        .then(({ data: { getVaultHabits: data } }) => {
+          this.habits = data
+          this.loading = false
+        })
     }
+  },
+  methods: {
+    ...mapActions({
+      fetchUserDetailsAction: 'user/fetchUserDetails',
+    }),
   },
 }
 </script>
